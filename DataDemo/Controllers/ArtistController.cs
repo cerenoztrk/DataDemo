1f8c5c3894dbf09f9b4cc773028777fd47e8759a
ceren.ozturk <ceren.ozturk@101-09>
2023-01-13 19:47:15 +0300

    Add project files.

---
+++ DataDemo/Controllers/ArtistController.cs
@@ -0,0 +1,45 @@
+﻿using DataDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataDemo.Controllers
+{
+    public class ArtistController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+
+            ChinookContext cnt = new ChinookContext();
+            return View(cnt.Artists.ToList());
+        }
+        public IActionResult AlbumByArtist(int id)
+        {
+            ChinookContext cnt = new ChinookContext();
+            List<Album> albums = cnt.Albums.Where(x=>x.ArtistId==id).ToList();
+            return View(albums);
+        }
+        // HTTP GET VERSION
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Create(Artist artist)
+        {
+            ChinookContext context = new ChinookContext();
+            context.Artists.Add(artist);
+            context.SaveChanges();
+            return View("Thanks", artist);
+
+            //Artist secilecek = new Artist();
+            //secilecek.ArtistId = intsecim;
+            //context.Artists.Add(secilecek);
+            //context.SaveChanges();
+        }
+
+
+    }
+}
